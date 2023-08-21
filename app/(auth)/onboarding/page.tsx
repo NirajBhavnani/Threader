66f798382912a5acf8506b7f03ce0a8e89b1117f
nbhavna <niraj.bhavnani@morningstar.com>
2023-08-22 03:04:22 +0530

    Onboarding-AccountProfile basic flow setup

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,38 @@
-async function Page(){
-    return(
-        <main>
-            <h1 className="head-text">Onboarding</h1>
-        </main>
-    )
+// Onboarding is basically a couple of steps you have to go through
+// as soon as you create an account
+
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
+async function Page() {
+  const user = await currentUser();
+
+  // This is coming from DB
+  const userInfo = {};
+
+  // We will have our own instance of user in DB so we can attach different threads to them
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  };
+
+  return (
+    <main className="mx-auto flex-max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now to use Threader
+      </p>
+
+      <section className="mt-9 bg-dark-2 p-10">
+        {/* For using AccountProfile, we have to provide current user info */}
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </section>
+    </main>
+  );
 }
 
 export default Page;
